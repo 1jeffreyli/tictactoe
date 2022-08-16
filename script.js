f22bb51cbb2a6bedfa6f37516ee8df3f6f333eec
Jeffrey Li <lijeffre@grinnell.edu>
2022-08-16 16:56:08 -0700

    Refactor modules

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 const gameBoard = (() => {
   const board = ["X", "X", "O", "O", "X", " ", " ", " ", " "];
-  const render = (array) => {
-    for (let i = 0; i < array.length; i++) {
-      const boardcontainer = document.querySelector(".gameboard");
-      const div = document.createElement("div");
-      div.classList.add("game-box");
-      div.innerHTML = array[i];
-      boardcontainer.appendChild(div);
-    }
-  };
 
   return {
-    board,
-    render,
+    board
   };
 })();
 
-const playerFactory = (name) => {
-  const sayName = () => {
-    console.log(name);
-  };
+const playerFactory = () => {
+    return {
 
-  return {
-    sayName,
   };
 };
 
-const displayController = (() => {})();
+const displayController = (() => {
+    const render = (array) => {
+        for (let i = 0; i < array.length; i++) {
+          const boardcontainer = document.querySelector(".gameboard");
+          const div = document.createElement("div");
+          div.classList.add("game-box");
+          div.innerHTML = array[i];
+          boardcontainer.appendChild(div);
+        }
+    }
+
+    return {
+        render
+    };
+}) ();
 
 // iterate through the board array and render it on
 // the page
-
-gameBoard.render(gameBoard.board);
+displayController.render(gameBoard.board);
