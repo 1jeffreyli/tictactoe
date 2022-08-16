76bf692ade3e8ee9377dc2922d79752f02f69139
Jeffrey Li <lijeffre@grinnell.edu>
2022-08-16 16:48:01 -0700

    Add prettier config file and add render function

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const gameBoard = (() => {
-    const playerMove = [];
-
-    return {
-        playerMove,
-
+  const board = ["X", "X", "O", "O", "X", " ", " ", " ", " "];
+  const render = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      const boardcontainer = document.querySelector(".gameboard");
+      const div = document.createElement("div");
+      div.classList.add("game-box");
+      div.innerHTML = array[i];
+      boardcontainer.appendChild(div);
     }
-}) ();
+  };
+
+  return {
+    board,
+    render,
+  };
+})();
 
 const playerFactory = (name) => {
-    const sayName = () => {
-        console.log(name);
-    }
+  const sayName = () => {
+    console.log(name);
+  };
 
-    return {
-        sayName
-    }
+  return {
+    sayName,
+  };
 };
 
+const displayController = (() => {})();
 
-const displayController = (() => {
+// iterate through the board array and render it on
+// the page
 
-
-}) ();
+gameBoard.render(gameBoard.board);
