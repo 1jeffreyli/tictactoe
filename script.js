24e2c4e543a803dec33a667d83f59d1fb35d860a
Jeffrey Li <lijeffre@grinnell.edu>
2022-08-17 21:32:44 -0700

    Add functioning turn system

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
 const gameBoard = (() => {
-  const board = ["X", "X", "O", "O", "X", " ", " ", " ", " "];
+  const board = ["", "", "", "", "", " ", " ", " ", " "];
 
   return {
-    board
+    board,
   };
 })();
 
 const playerFactory = (() => {
-  let currentTurn = [];
-  const promptPlayer = () => {
-    let playerChoice = prompt("X or O?");
-    let playerChoiceString = playerChoice.toString();
-    currentTurn[0] = playerChoiceString;
-  }
-    return {
-      currentTurn,
-      promptPlayer
+  let turn = true;
+  const changeTurn = () => {
+    if (turn === true) {
+      turn = false;
+      currentTurn[0] = "X";
+    } else {
+      turn = true;
+      currentTurn[0] = "O";
+    }
   };
-}) ();
+  let currentTurn = [];
+
+  return {
+    currentTurn,
+    changeTurn,
+  };
+})();
 
 const displayController = (() => {
-    const render = (array) => {
-        for (let i = 0; i < array.length; i++) {
-          const boardcontainer = document.querySelector(".gameboard");
-          const div = document.createElement("div");
-          div.classList.add("game-box");
-          div.innerHTML = array[i];
-          boardcontainer.appendChild(div);
-        }
-        const boardcontainer = document.querySelector(".gameboard");
-        boardcontainer.addEventListener("click", changeColor)
+  const render = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      const boardcontainer = document.querySelector(".gameboard");
+      const div = document.createElement("div");
+      div.classList.add("game-box");
+      div.innerHTML = array[i];
+      boardcontainer.appendChild(div);
     }
-    const changeColor = (event) => {
-      const target = event.target;
-      if (target.matches("div")) {
-        // target.style.backgroundColor = "red";
-        if (target.innerHTML === "O" || target.innerHTML === "X") {
-          alert("Sorry, please choose a different square");
-        } else if (target.innerHTML != "O" && target.innerHTML != "X") {
-          playerFactory.promptPlayer();
-          target.innerHTML = playerFactory.currentTurn[0];
-        }
-        }
-      };
-    return {
-        render,
-        changeColor
-    };
-}) ();
+    const boardcontainer = document.querySelector(".gameboard");
+    boardcontainer.addEventListener("click", addMarker);
+  };
+  const addMarker = (event) => {
+    const target = event.target;
+    if (target.matches("div")) {
+      if (target.innerHTML === "O" || target.innerHTML === "X") {
+        alert("Sorry, please choose a different square");
+      } else if (target.innerHTML != "O" && target.innerHTML != "X") {
+        playerFactory.changeTurn();
+        target.innerHTML = playerFactory.currentTurn[0];
+      }
+    }
+  };
+  return {
+    render,
+  };
+})();
 
 // iterate through the board array and render it on
 // the page
