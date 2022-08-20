985d5c7b2fde6c10a3f837343f62abc56b1d4e9d
Jeffrey Li <lijeffre@grinnell.edu>
2022-08-19 17:27:59 -0700

    Clean up code from scratch

--- script.js
+++ script.js
@@ -1,74 +1,47 @@
+const Player = (name, character) => {
+  this.name = name;
+  this.character = character;
+  function getName () {
+    return name;
+  }
+  function getCharacter () {
+    return character;
+  }
+  return { getName, getCharacter }
+  }
+
 const gameBoard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
-  const boardcontainer = document.querySelector(".gameboard");
-  const boxes = document.querySelectorAll(".game-box");
-  // const updateBoard = () => {
-  //   const boxesArr = Array.from(boxes);
-  //   boxesArr.forEach(box => {
-  //     box.addEventListener("click", event => {
-  //       let index = boxesArr.indexOf(box);
-  //     })
-  //   }
-  // }
+  const boardContainer = document.querySelector(".gameboard");
+  
   return {
     board,
-    boardcontainer,
-    boxes,
-    // updateBoard
-  };
-})();
-
-const playerFactory = (() => {
-  let turn = true;
-  function changeTurn() {
-    if (turn === true) {
-      turn = false;
-      currentTurn[0] = "X";
-    } else {
-      turn = true;
-      currentTurn[0] = "O";
-    }
+    boardContainer,
+    
   }
-  let currentTurn = [];
-
-  return {
-    currentTurn,
-    changeTurn,
-  };
-})();
+}) ();
 
 const displayController = (() => {
-  function render(array) {
+  function render (array) {
     reset();
     gameBoard.board.forEach((cell) => {
       const div = document.createElement("div");
       div.classList.add("game-box");
-      div.innerHTML = cell;
-      gameBoard.boardcontainer.appendChild(div);
-    });
-    gameBoard.boardcontainer.addEventListener("click", addMarker);
+      div.textContent = cell;
+      gameBoard.boardContainer.appendChild(div);
+    })
   }
-  function reset() {
-    gameBoard.boardcontainer.innerHTML = "";
-  }
-  
-  function addMarker(event) {
-    const target = event.target;
-    if (target.matches("div")) {
-      if (target.innerHTML === "O" || target.innerHTML === "X") {
-        alert("Sorry, please choose a different square");
-      } else if (target.innerHTML != "O" && target.innerHTML != "X") {
-        playerFactory.changeTurn();
-        target.innerHTML = playerFactory.currentTurn[0];
-      }
-    }
+
+  function reset () {
+    gameBoard.boardContainer.innerHTML = "";
   }
   return {
-    render,
-  };
-})();
+    render
+  }
+}) ();
 
-// iterate through the board array and render it on
-// the page
-// displayController.render(gameBoard.board);
-gameBoard.boardcontainer.addEventListener("click", displayController.render(gameBoard.board));
+const gameController = (() => {
+
+}) ();
+
+displayController.render(gameBoard.board);
